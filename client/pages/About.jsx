2129028cffd66ecdcb59c67dcff3f634a576f9ce
kaseywolff <kaseywolff5@gmail.com>
2023-07-03 09:21:42 -0500

    about styling

--- client/pages/About.jsx
+++ client/pages/About.jsx
@@ -15,14 +15,21 @@ function About() {
 
 
         {/* <div className='text-a'> */}
-          <div className='title-a'>About</div>
           <div className='aboutInfo'>
+          <div className='title-a'>About</div>
             <p className='p1'>Hello! I'm Kasey.</p>
 
-            <p className='p3'>I am a Software Engineer who loves tackling and solving complex problems. I am someone who approaches new things with a sense of curiosity, wonder, and a genuine desire to understand how things work.</p>
+            <p className='p2'>
+              I am a Software Engineer who loves tackling and solving complex problems. I am someone who approaches new things with a sense of curiosity, wonder, and a genuine desire to understand how things work.
+            </p>
+            <p className='p3'>
+              
+            </p>
           </div>
         {/* </div> */}
       </div>
+
+      {/* <img className='profilePhoto' src={profile} alt={'headshot'} /> */}
 
     </div>
   );
