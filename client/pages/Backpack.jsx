bf382377fd76895e079d8f8854fa7b3b39c585c6
kaseywolff <kaseywolff5@gmail.com>
2023-07-05 23:46:33 -0500

    backpack

--- client/pages/Backpack.jsx
+++ client/pages/Backpack.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
+import '../styles/backpack.scss';
 // import images
-// import background from '../assets/images'
+import background from '../assets/images/backpack.jpg'
 
 
 function Backpack() {
   return(
     <div className='backpack'>
-      {/* <img src={background} alt='background image' className='bg-b' /> */}
+      <img src={background} alt='background image' className='bg-b' />
 
       <div className='scrollDiv-b'>
-        {/* <div className='project-container'>
+        {/* <h1>Tools and </h1> */}
+        <div className='project-container'>
+          <h2>Projects</h2>
 
-        </div> */}
+        </div>
 
         <div className='techStack-container'>
-          
+          <h3>Tech Stack</h3>
         </div>
       </div>
     </div>
