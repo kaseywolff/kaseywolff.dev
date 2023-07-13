a2e64ee0e488ce5f103063f0210a6b73501b6d8b
kaseywolff <kaseywolff5@gmail.com>
2023-07-13 13:52:22 -0500

    map

--- client/pages/Map.jsx
+++ client/pages/Map.jsx
@@ -3,13 +3,21 @@ import Footer from '../components/Footer';
 import '../styles/map.scss';
 // import images
 // import background from '../assets/images/canyonlands-road.jpg';
-import background from '../assets/images/joshuatree-stars.jpg';
+import background from '../assets/images/old-snowy-topo2.png';
 
 function Map() {
   return(
     <div id='map'>
       <img src={background} alt='background image' id='bg-m' />
 
+      <div id='resume'>
+        <div id='current'>
+          <div className='job-title'>Recoilize</div>
+          <div className='job-description'>
+            <p></p>
+          </div>
+        </div>
+      </div>
       
     </div>
   );
