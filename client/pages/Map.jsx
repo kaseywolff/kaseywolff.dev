1bfc46319693f2513076e5e8f0276a368143b275
kaseywolff <kaseywolff5@gmail.com>
2023-07-13 13:19:24 -0500

    map

--- client/pages/Map.jsx
+++ client/pages/Map.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import Footer from '../components/Footer';
 import '../styles/map.scss';
 // import images
-import background from '../assets/images/canyonlands-road.jpg';
+// import background from '../assets/images/canyonlands-road.jpg';
+import background from '../assets/images/joshuatree-stars.jpg';
 
 function Map() {
   return(
     <div id='map'>
       <img src={background} alt='background image' id='bg-m' />
+
+      
     </div>
   );
 };
