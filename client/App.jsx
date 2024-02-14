67a1ee016ad979c4f0a7e018a03d91baecf311ce
kaseywolff <kaseywolff5@gmail.com>
2024-02-14 16:09:02 -0600

    updated nav bar to change color

--- client/App.jsx
+++ client/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Route, Routes } from 'react-router';
+import { Route, Routes, useLocation } from 'react-router';
 import NavBar from './components/NavBar';
 import HomePage from './pages/Home';
 import About from './pages/About';
 import Backpack from  './pages/Backpack';
 
 function App() {
+  const location = useLocation();
+
   return (
     <>
-      <NavBar />
+      <NavBar currentLocation={location.pathname}/>
       <div className='container'>
         <Routes>
           <Route path='/' element={<HomePage />} />
--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-function NavBar() {
+function NavBar({ currentLocation }) {
   return(
   <div className="navBar">
-    <NavLink to='/' className='navLink'>home</NavLink>
-    <NavLink to='/about' className='navLink'>about</NavLink>
-    <NavLink to='/backpack' className='navLink' data-replace='backpack'>backpack</NavLink>
+    <NavLink to='/' className={`navLink ${currentLocation === '/' ? 'active' : ''}`}>home</NavLink>
+    <NavLink to='/about' className={`navLink ${currentLocation === '/about' ? 'active' : ''}`}>about</NavLink>
+    <NavLink to='/backpack' className={`navLink ${currentLocation === '/backpack' ? 'active' : ''}`}>backpack</NavLink>
   </div>
   );
 };
