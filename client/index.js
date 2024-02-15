4482844cb1c0f876393dbd6b008ea54128592560
kaseywolff <kaseywolff5@gmail.com>
2024-02-15 13:20:29 -0600

    analytics added, deployed

--- client/index.js
+++ client/index.js
@@ -1,11 +1,11 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import { BrowserRouter } from "react-router-dom"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
-import App from "./App"
-import styles from './styles.scss'
+import App from './App';
+import './styles.scss';
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
@@ -13,4 +13,4 @@ root.render(
       <App />
     </BrowserRouter>
   </React.StrictMode>
-)
+);
