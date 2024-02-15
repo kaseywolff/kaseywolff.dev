2c2fb22e1432a79335d8748d089920ecf4c009f6
kaseywolff <kaseywolff5@gmail.com>
2024-02-15 14:08:55 -0600

    improved performance, smaller image sizes

--- client/pages/Home.jsx
+++ client/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '../styles/home.scss';
 // import images
 // import background from '../assets/images/joshua-tree.jpg';
-import main from '../assets/images/main-cover.png';
+import main from '../assets/images/home-geo.png';
 import overlays from '../assets/images/geo-back.png';
 // import footer
 import Footer from '../components/Footer'
@@ -12,9 +12,6 @@ import Footer from '../components/Footer'
 function HomePage() {
   return(
     <div className='homePage'>
-
-      {/* <img src={background} alt='background image' className='backgroundPhoto' /> */}
-
       <div className='scrollDiv'>
         <div id='geoGroup'>
           <img src={main} alt='main image' id='mainPhoto' />
