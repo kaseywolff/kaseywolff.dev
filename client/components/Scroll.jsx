9f4649620cb2539d60f1a4e8cb734aba00120915
kaseywolff <kaseywolff5@gmail.com>
2024-02-15 21:48:51 -0600

    adjustments to scroll button

--- client/components/Scroll.jsx
+++ client/components/Scroll.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 export default function Scroll() {
   const [isVisible, setIsVisible] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
 
   // check scroll position and show/hide the button accordingly
   useEffect(() => {
@@ -21,19 +22,28 @@ export default function Scroll() {
 
   // scroll to the top when the button is clicked
   const scrollToTop = () => {
+    setIsClicked(true);
+
     window.scrollTo({
       top: 0,
       behavior: 'smooth',
     });
+
+    // delay resetting the state -> prevents button from momentarily disappearing and reappearing after reaching top of page
+    setTimeout(() => {
+      setIsClicked(false);
+    }, 1000);
   };
 
   return (
     <button
-      className={`scroll ${isVisible ? 'visible' : 'hidden'}`}
+      className={`scroll ${isVisible ? 'visible' : 'hidden'} ${
+        isClicked ? 'clicked' : ''
+      }`}
       onClick={scrollToTop}
       title='scroll to top'
     >
-      🛸
+      {isClicked ? 'woosh!' : '🛸'}
     </button>
   );
 };
