46cd35907b6fabe5e99ac14f67d6dfabec7df6ed
kaseywolff <kaseywolff5@gmail.com>
2024-04-05 10:58:54 -0500

    fixed firebase analytics, added python logo

--- client/index.js
+++ client/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
+import './firebase';
 import './styles.scss';
 import './styles/nav-footer.scss';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
--- client/pages/Backpack.jsx
+++ client/pages/Backpack.jsx
@@ -21,6 +21,7 @@ import next from '../assets/logos/next-logo.png';
 import node from '../assets/logos/node-logo.png';
 import npm from '../assets/logos/npm-square-logo.png';
 import postgres from '../assets/logos/PostgreSQL-logo.png';
+import python from '../assets/logos/python-logo.png';
 import reactLogo from '../assets/logos/react-logo.png';
 import reactRouter from '../assets/logos/react-router-logo.png';
 import redux from '../assets/logos/redux-logo.png';
@@ -31,7 +32,7 @@ import ts from '../assets/logos/ts-logo-256.png';
 
 
 function Backpack() {
-  return(
+  return (
     <div className='page'>
       <div className='container backpack'>
         <h1 className='header-backpack'>backpack</h1>
@@ -39,10 +40,10 @@ function Backpack() {
       </div>
 
       <div className='blank'></div>
-        
+
       <div className='container projects'>
         <h2>projects</h2>
-        
+
         <div className='primary'>
           <div className='card project-card'>
             <h3 href='https://www.recoilize.io/' className='project-name'>Recoilize</h3>
@@ -50,28 +51,28 @@ function Backpack() {
             <p>
               Recoilize is a Chrome Dev Tool designed for debugging applications built with the Recoil.js state management library.
             </p>
-            <br/>
+            <br />
             <p>
               <a href='https://chrome.google.com/webstore/detail/recoilize/jhfmmdhbinleghabnblahfjfalfgidik' className='hyperlink'>  chrome extension  </a>
-                | 
+              |
               <a href='https://www.npmjs.com/package/recoilize' className='hyperlink'>  npm package  </a>
             </p>
           </div>
 
-            <div className='card project-card'>
-              <h3 className='project-name'>Codesweeper</h3>
-              <img src={codesweeper} className='project-screenshot' />
-              <p>
-                An elevated, cleaner version of the classic minesweeper game - for coders.
-              </p>
-              <br/>
-              <p>
-                <a href='https://codesweeper.app' className='hyperlink'>  play game  </a>
-                | 
-                <a href='https://https://github.com/kaseywolff/Codesweeper' className='hyperlink'>  github code  </a>
-              </p>
-            </div>
+          <div className='card project-card'>
+            <h3 className='project-name'>Codesweeper</h3>
+            <img src={codesweeper} className='project-screenshot' />
+            <p>
+              An elevated, cleaner version of the classic minesweeper game - for coders.
+            </p>
+            <br />
+            <p>
+              <a href='https://codesweeper.app' className='hyperlink'>  play game  </a>
+              |
+              <a href='https://https://github.com/kaseywolff/Codesweeper' className='hyperlink'>  github code  </a>
+            </p>
           </div>
+        </div>
 
         <div className='secondary'>
           <div className='card project-card'>
@@ -97,27 +98,28 @@ function Backpack() {
       <div className='container tech-stack'>
         <h2>tech stack</h2>
         <div className='card logo-card'>
-          <img src={js} alt='javascript logo' className='logo' title='javascript'/>
-          <img src={ts} alt='typescript logo' className='logo' title='typescript'/>
-          <img src={reactLogo} alt='react logo' className='logo' title='react'/>
-          <img src={reactRouter} alt='react router logo' className='logo' title='react router'/>
-          <img src={next} alt='next.js logo' className='logo' title='next.js'/>
-          <img src={redux} alt='redux logo' className='logo' title='redux'/>
-          <img src={express} alt='express.js logo' className='logo' title='express'/>
-          <img src={node} alt='node.js logo' className='logo' title='node.js'/>
-          <img src={mongodb} alt='mongodb logo' className='logo' title='mongodb'/>
-          <img src={postgres} alt='postgreSQL logo' className='logo' title='postreSQL'/>
-          <img src={jest} alt='jest logo' className='logo' title='jest'/>
-          <img src={testing} alt='react testing library logo' className='logo' title='react testing library'/>
-          <img src={npm} alt='npm logo' className='logo' title='npm'/>
-          <img src={git} alt='git logo' className='logo' title='git'/>
-          <img src={aws} alt='aws logo' className='logo' title='aws'/>
-          <img src={docker} alt='docker logo' className='logo' title='docker'/>
-          <img src={firebase} alt='firebase logo' className='logo' title='firebase'/>
-          <img src={html5} alt='html5 logo' className='logo' title='html5'/>
-          <img src={css} alt='css logo' className='logo' title='css'/>
-          <img src={sass} alt='scss logo' className='logo' title='scss'/>
-          <img src={d3} alt=' d3 logo' className='logo' title='d3'/>
+          <img src={js} alt='javascript logo' className='logo' title='javascript' />
+          <img src={ts} alt='typescript logo' className='logo' title='typescript' />
+          <img src={python} alt='python logo' className='logo' title='python' />
+          <img src={reactLogo} alt='react logo' className='logo' title='react' />
+          <img src={reactRouter} alt='react router logo' className='logo' title='react router' />
+          <img src={next} alt='next.js logo' className='logo' title='next.js' />
+          <img src={redux} alt='redux logo' className='logo' title='redux' />
+          <img src={express} alt='express.js logo' className='logo' title='express' />
+          <img src={node} alt='node.js logo' className='logo' title='node.js' />
+          <img src={mongodb} alt='mongodb logo' className='logo' title='mongodb' />
+          <img src={postgres} alt='postgreSQL logo' className='logo' title='postreSQL' />
+          <img src={jest} alt='jest logo' className='logo' title='jest' />
+          <img src={testing} alt='react testing library logo' className='logo' title='react testing library' />
+          <img src={npm} alt='npm logo' className='logo' title='npm' />
+          <img src={git} alt='git logo' className='logo' title='git' />
+          <img src={aws} alt='aws logo' className='logo' title='aws' />
+          <img src={firebase} alt='firebase logo' className='logo' title='firebase' />
+          <img src={docker} alt='docker logo' className='logo' title='docker' />
+          <img src={html5} alt='html5 logo' className='logo' title='html5' />
+          <img src={css} alt='css logo' className='logo' title='css' />
+          <img src={sass} alt='scss logo' className='logo' title='scss' />
+          <img src={d3} alt=' d3 logo' className='logo' title='d3' />
         </div>
       </div>
     </div>
