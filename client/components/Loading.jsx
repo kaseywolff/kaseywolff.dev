e71f2e72db8f086587ad335406d0eb22cad6c21f
kaseywolff <kaseywolff5@gmail.com>
2024-04-05 16:01:44 -0500

    animated loading component

--- client/components/Loading.jsx
+++ client/components/Loading.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import '../styles/about.scss';
-
+import tree from '../assets/images/tech-tree(fawn).png';
 
 export default function Loading() {
   return (
-    <div className='loading'>
-      &lt; Loading... /&gt;
+    <div className='loading-container'>
+      <img id='loading-img' src={tree} alt="Loading" />
+      <div className='loading-text'>
+        {/* animation in styles.scss file */}
+      </div>
     </div>
-  )
-}
+  );
+};
